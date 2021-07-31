020552de425c8d5a11d2da6535a36f45c87423ef
go-tour <go-tour@program>
2021-08-01 02:06:43 +0300

    i remove no-history push from program.sh

--- program.sh
+++ program.sh
@@ -13,16 +13,4 @@ install_golang(){
     && go version
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am go-tour program"
-  git push -f -u origin main
-}
-
-
 "$@"

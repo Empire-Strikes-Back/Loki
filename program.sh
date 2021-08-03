77e556aebe12ef94132f605e930be0304fcb2c0e
go-tour <go-tour@program>
2021-08-03 20:12:54 +0300

    i revert "for creating uberjar i use genie library"
    This reverts commit 00e1028fc4e82111d6c6486652e22ebbc58d08a8.

--- program.sh
+++ program.sh
@@ -17,13 +17,4 @@ main(){
     -M -m go-tour.main
 }
 
-uberjar(){
-  clj \
-    -X:uberjar genie.core/process \
-    :uberjar-name out/go-tour.standalone.jar \
-    :main-ns go-tour.main
-  mkdir -p out/jpackage-input
-  mv out/go-tour.standalone.jar out/jpackage-input/
-}
-
 "$@"
